53acc46ddaa28d2afd81d4dda531a27571e7654c
Vipul Tanwar <vipultanwar45@gmail.com>
2020-12-01 16:34:50 +0530

    Express Session Part 2

---
+++ models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+const Promos = require("./promos");
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('User', User);
